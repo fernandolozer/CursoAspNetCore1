0ead7743887fd91579393bae190e6825b66caf33
Fernando Lozer Lichtenheld <flichtenheld@gmail.com>
2016-04-02 06:57:52 -0700

    Mais algumas classes

---
+++ LinguagemCS6/LinguagemCS6/ExpressoesNameOf.cs
@@ -0,0 +1,57 @@
+﻿using static System.Console;
+using System;
+using System.ComponentModel;
+
+namespace LinguagemCS6
+{
+    class ExpressoesNameOf
+    {
+        static void Main(string[] args)
+        {
+            string factory = "microsoft";
+            DateTime lancto = new DateTime(2015, 5, 15);
+            WriteLine($"fabricante: {factory} - lançamento: {lancto}");
+            WriteLine($"fabricante: {nameof(factory)} - lançamento: {nameof(lancto)}");
+
+            var c = new carro2 { ID = 1, Modelo = "bmw X3", Ano = 2012, Cor = "azul" };
+            WriteLine($"{nameof(c.Modelo)}: {c.Modelo}");
+
+            Console.ReadLine();
+        }
+    }
+
+    public class carro2
+    {
+        //if (modelo == null)
+        //    throw new ArgumentException("modelo");
+        public int ID { get; set; }
+        public string Modelo { get; set; }
+        public int Ano { get; set; }
+        public string Cor { get; set; }
+    }
+
+    class Livro : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        private string _titulo;
+
+        public string Titulo
+        {
+            get { return _titulo ?? ""; }
+            set
+            {
+                if (_titulo != value)
+                {
+                    _titulo = value;
+                    OnPropertyChanged(nameof(Titulo));
+                };
+            }
+        }
+
+        protected virtual void OnPropertyChanged(string livro)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(livro));
+        }
+    }
+}
---
+++ LinguagemCS6/LinguagemCS6/FiltrosException.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+using System.Security;
+using System.Data.SqlClient;
+
+namespace LinguagemCS6
+{
+    class FiltrosException
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                // algum código
+                throw new Exception("tratar erro");
+            }
+            catch (Exception ex) when (ex.GetType() == typeof(SqlException))
+            {
+                Console.WriteLine("erro customizado do SQL");
+            }
+            catch (Exception ex) when (ex.Message.Equals("tratar erro", StringComparison.CurrentCultureIgnoreCase))
+            {
+                Console.WriteLine("erro customizado da mensagem");
+            }
+            catch (Exception ex) when (ex is SecurityException || ex is IOException)
+            {
+                Console.WriteLine("erro de segurança ou IO");
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("erro genérico");
+            }
+        }
+    }
+}
