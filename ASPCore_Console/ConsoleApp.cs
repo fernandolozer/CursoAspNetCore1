f60e442be96c4447eb981dac89de6ebcead5e881
Fernando Lozer <fernando.lozer@phidelis.com.br>
2016-04-02 14:00:20 -0300

    Commit parcial

---
+++ ASPCore_Console/ConsoleApp.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace ConsoleApp1{
+    public class Program {
+        public static void Main (string[] args)
+        {
+            Console.WriteLine("Hello from DNX!!!");
+        }
+    }
+}
